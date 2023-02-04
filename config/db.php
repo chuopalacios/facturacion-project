b35ac7aacee15cbd6bef44ad5ddc68ba3039335c
chuopalacios <chuox593@gmail.com>
2023-02-04 17:52:31 -0500

    conexion base de de datos modificada

--- config/db.php
+++ config/db.php
@@ -2,6 +2,6 @@
 /*Datos de conexion a la base de datos*/
 define('DB_HOST', 'mysql-chuox.alwaysdata.net');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'chuox');//Usuario de tu base de datos
-define('DB_PASS', 'daniel123456_');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'chuox_simple_invoice');//Nombre de la base de datos
+define('DB_PASS', 'iamdanielpdc123_');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'chuox_simple-invoice');//Nombre de la base de datos
 ?>
